5448ee9ff055b3a90d0c66f96e4b3c43f689fe72
samb1232 <arterm212@gmail.com>
2025-04-02 18:34:49 +0500

    Initial commit

---
+++ src/test/java/com/samb1232/urfu_java_bot/UrfuJavaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samb1232.urfu_java_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrfuJavaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

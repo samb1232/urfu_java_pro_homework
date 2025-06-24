49e39aec3a39cfcd12417e877c895b1c0d6f81cb
samb1232 <arterm212@gmail.com>
2025-06-24 20:20:50 +0500

    moved tg api functionality to TelegramApiService

--- src/main/java/com/samb1232/urfu_java_bot/tg_bot/MainBot.java
+++ src/main/java/com/samb1232/urfu_java_bot/tg_bot/MainBot.java
@@ -5,45 +5,34 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class MainBot extends TelegramLongPollingBot {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(MainBot.class);
+    private final TelegramApiService telegramApiService;
 
-    public MainBot(@Value("${bot.token}")String botToken) {
+    public MainBot(@Value("${bot.token}") String botToken) {
         super(botToken);
+        this.telegramApiService = new TelegramApiService(this);
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (!update.hasMessage() || !update.getMessage().hasText()){
+        if (!update.hasMessage() || !update.getMessage().hasText()) {
             return;
         }
         Message msg = update.getMessage();
         sendEchoMessage(msg);
-
     }
 
     private void sendEchoMessage(Message msg) {
         var messageText = msg.getText();
         var chatId = msg.getChatId();
-        sendMessage(chatId, messageText);
+        telegramApiService.sendMessage(chatId, messageText);
     }
-    private void sendMessage(Long chatId, String text) {
-        var chatIdStr = String.valueOf(chatId);
-        var sendMessage = new SendMessage(chatIdStr, text);
 
-        try {
-            execute(sendMessage);
-        } catch (TelegramApiException e) {
-            LOGGER.error("Error sending message", e);
-        }
-    }
     @Override
     public String getBotUsername() {
         return "urfu_java_dz_bot";
---
+++ src/main/java/com/samb1232/urfu_java_bot/tg_bot/TelegramApiService.java
@@ -0,0 +1,28 @@
+package com.samb1232.urfu_java_bot.tg_bot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class TelegramApiService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TelegramApiService.class);
+    private final TelegramLongPollingBot bot;
+
+    public TelegramApiService(TelegramLongPollingBot bot) {
+        this.bot = bot;
+    }
+
+    public void sendMessage(Long chatId, String text) {
+        var chatIdStr = String.valueOf(chatId);
+        var sendMessage = new SendMessage(chatIdStr, text);
+
+        try {
+            bot.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            LOGGER.error("Error sending message", e);
+        }
+    }
+}

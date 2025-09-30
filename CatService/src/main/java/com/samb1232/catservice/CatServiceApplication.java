c7dabf71bcc2c8f3971fc085fa32392a55cbfd11
samb1232 <arterm212@gmail.com>
2025-09-30 15:59:09 +0500

    added cat service placeholder

---
+++ CatService/src/main/java/com/samb1232/catservice/CatServiceApplication.java
@@ -0,0 +1,14 @@
+package com.samb1232.catservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatServiceApplication.class, args);
+    }
+}
+
+
